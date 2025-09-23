6acca3102f25982c3700dcb9db9bc6bf1e3d28cc
Pedrozle <pedrozle@outlook.com>
2025-09-23 09:00:00 -0400

    refac: altera visual

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,15 @@
-fetch("./assets/modules/header.html")
+fetch("./../assets/modules/header.html")
   .then((res) => res.text())
-  .then((html) => (document.getElementById("header").innerHTML = html));
+  .then((html) => {
+    document.getElementById("header").innerHTML = html;
+    const path =
+      window.location.pathname.replace(/^\//, "").split("/")[0] || "início";
+    const breadRoute = document.getElementById("bread-route");
+    if (breadRoute) {
+      breadRoute.textContent = path.charAt(0).toUpperCase() + path.slice(1);
+    }
+  });
 
-fetch("./assets/modules/footer.html")
+fetch("./../assets/modules/footer.html")
   .then((res) => res.text())
   .then((html) => (document.getElementById("footer").innerHTML = html));
